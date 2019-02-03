ac525d6fda3fb3e3a72874b23a40d285f80dfc5d
Nick Visalli <nick@Nicks-MacBook-Pro.local>
2019-02-02 23:08:07 -0500

    create stream reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
-    form: formReducer
+    form: formReducer,
+    streams: streamReducer
 });
