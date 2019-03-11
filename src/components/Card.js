c7d28757c2e9dfaf91e9371e6b5d1ee5deff9727
Nick Visalli <nvisal1@students.towson.edu>
2019-03-11 10:21:25 -0400

    create team cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,6 @@ const Card = (props) => {
                 <img style={{width:'100%'}}src={props.stream.thumbnail}></img>
                 <div class="container">
                     <h4><b>{props.stream.title}</b></h4> 
-                    <p>{props.stream.description}</p> 
                 </div>
             </div>
         </Link>
